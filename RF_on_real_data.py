aa72f1e4152123e2147c0c119e159bfbdaad3df4
Linas.Pocius <linas.pocius@ktu.edu>
2023-11-08 22:30:26 +0200

    Add docs and minor fixes

--- RF_on_real_data.py
+++ RF_on_real_data.py
@@ -12,10 +12,10 @@ def remove_dataframe_features_with_all_null_values(dataframe):
 df = pd.read_json("subset_normalized_json_file_with_scaler.json")
 df = remove_dataframe_features_with_all_null_values(df)
 
-X = df.drop(columns=['elliptic_label']) # ismetam nereikalingus
+X = df.drop(columns=['elliptic_label']) # delete unused
 y = df['elliptic_label']
 
-# legalios = 0, nelegalios = 1
+# legal = 0, illegal = 1
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25,random_state=15)
 
@@ -23,7 +23,7 @@ model_RF = RandomForestClassifier().fit(X_train.values,y_train.values)
 y_preds = model_RF.predict(X_test.values)
 
 accuracy = accuracy_score(y_test, y_preds)
-print("Tikslumas:", accuracy)
+print("Accuracy:", accuracy)
 
 filename = 'RF_model.sav'
 dump(model_RF, open(filename, 'wb'))
