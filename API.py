7e391c0a41d6ead4d21988644008f49f7e5ef5ad
Linas.Pocius <linas.pocius@ktu.edu>
2023-11-22 19:44:42 +0200

    Improve API

--- API.py
+++ API.py
@@ -1,19 +1,13 @@
-import math
 from fastapi import FastAPI
-import requests
-import json
-import pandas as pd
-import requests
-from flatten_json import flatten
 from pickle import load
 from fastapi.middleware.cors import CORSMiddleware
+from predict_transaction import get_tx_data, get_wallet_data, predict, preprocess_tx
+
 origins = [
     "http://localhost",
     "http://localhost:3000",
 ]
 
-features_json_file = "all_features.json"
-model_file = "RF_model.sav"
 
 app = FastAPI()
 
@@ -25,80 +19,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+model_file = "RF_model.sav"
+scaler_file = "scaler.pkl"
 model_RF = load(open(model_file, 'rb'))
-scaler = load(open('scaler.pkl', 'rb'))
-
-
-# ----------------------------------------------------------------------------------------
-# Real data transaction prediction
-# Tx hash for prediction: 4f4ddca2436e5c3f9ecda31a2d1d3209d3f1658e5845bf5d6dfb46c0dc6f1a4b
-# Known bad tx: 48cc5af8141a7be7b396029e5093a9f0fe78ea03076ebd4bc805bd977e93fbcc
-# ----------------------------------------------------------------------------------------
-
-def get_tx_data (hash):
-    try:
-        response = requests.get(f"https://blockchain.info/rawtx/{hash}")
-        tx_json = response.json()
-        return tx_json
-    except Exception:
-        print("Failed to get the Tx data")
-
-def get_wallet_data (addr):
-    try:
-        response = requests.get(f"https://blockchain.info/rawaddr/{addr}")
-        wallet_json = response.json()
-        return wallet_json
-    except Exception:
-        print("Failed to get the wallet data")
-
-def preprocess_tx (tx):
-    #flatten
-    tx = flatten(tx)
-
-    # Make bool binary
-    for key, value in tx.items():
-      if isinstance(value, bool):
-        value = int(value)
-      tx[key] = value
-
-    # Remove string features
-    tx_without_strings = {}
-    for key, value in tx.items():
-        if isinstance(value, int) or isinstance(value, float):
-            tx_without_strings[key] = value
-
-    # Add missing features
-    with open(features_json_file, "r") as f:
-        json_features = f.read()
-    features = json.loads(json_features)
-
-    for feature in features:
-        if feature not in tx_without_strings and feature != 'elliptic_label':
-            tx_without_strings[feature] = 0
-
-    # Remove if it has additional features
-    new_tx = {}
-    for key, value in tx_without_strings.items():
-        if key in features:
-            new_tx[key] = value
-
-    df = pd.DataFrame([new_tx])
-    tx = scaler.transform(df)
-
-    return (tx)
+scaler = load(open(scaler_file, 'rb'))
 
 @app.get("/transaction/")
 async def transaction_legality(txHash: str = None):
     if not txHash:
         return {"No txHash"}
     rawTx = get_tx_data(txHash)
-    tx = preprocess_tx(rawTx)
-
-    y = model_RF.predict(tx)[0]
-
-    predicted = True
-    if y == 1:
-        predicted = False
+    predicted = predict(txHash, scaler, model_RF)
 
     return {"txHash": txHash, "isLegal": predicted, "rawTx": rawTx}
 
@@ -110,7 +41,7 @@ async def wallet_legality(walletAddr: str = None):
 
     result = []
     for wallet_tx in wallet['txs']:
-        tx = preprocess_tx(wallet_tx)
+        tx = preprocess_tx(wallet_tx, scaler)
         y = model_RF.predict(tx)[0]
 
         predicted = True
