e0a49162a73452b6febc8ad1d8928833b360d243
Linas.Pocius <linas.pocius@ktu.edu>
2025-01-23 19:29:18 +0200

    Flatten transactions in the main fetching file

--- create_deanonymized_dataset.py
+++ create_deanonymized_dataset.py
@@ -3,7 +3,8 @@ import requests
 import json
 import argparse
 import os
-
+from flatten_json import flatten
+from utils import logInfo
 
 def parseArguments():
     # Create argument parser
@@ -11,6 +12,7 @@ def parseArguments():
 
     # Optional arguments
     parser.add_argument("-n", "--limitUnknown", help="Number of unknown transactions to get from API", type=int, default=1000)
+    parser.add_argument("-s", "--skipAPIFetch", help="Skip API fetching, only flatten", action='store_true', default=False)
 
     # Parse arguments
     args = parser.parse_args()
@@ -29,9 +31,7 @@ def getRealTransactionData(limitUnknown: int):
     5. Writes the fetched transaction data to JSON files.
     """
 
-    print(f"-----------------------------------------")
-    print(f"Reading dataset files")
-    print(f"-----------------------------------------")
+    logInfo(f"Reading dataset files")
 
     classes = pd.read_csv('elliptic_bitcoin_dataset/elliptic_txs_classes.csv')
     deanonym = pd.read_csv("elliptic_bitcoin_dataset/deanonymized_result.csv")
@@ -46,22 +46,22 @@ def getRealTransactionData(limitUnknown: int):
     bad_txs = labeled_txs[labeled_txs['class'] == "1"]
     unknown_txs = labeled_txs[labeled_txs['class'] == "3"]
 
-    print(f"good_tx_list length: {len(good_txs)}")
-    print(f"bad_tx_list length: {len(bad_txs)}")
-    print(f"unknown_txs_list length: {len(unknown_txs)}")
-    print(f"Total length: {len(labeled_txs)}")
+    logInfo(f"good_tx_list length: {len(good_txs)}")
+    logInfo(f"bad_tx_list length: {len(bad_txs)}")
+    logInfo(f"unknown_txs_list length: {len(unknown_txs)}")
+    logInfo(f"Total length: {len(labeled_txs)}")
 
 
     if not os.path.exists("pretrain/elliptic_txs.json"):
         json_list = []
         txs_failed_list = []
 
-        print(f"-----------------------------------------")
-        print(f"Getting legal transaction data from API")
-        print(f"-----------------------------------------")
+
+        logInfo(f"Getting legal transaction data from API")
+
         # legal transactions
         for i in range(len(good_txs)):
-            print(f"Legal txs [{i}/{len(good_txs)}]", end='\r')
+            logInfo(f"Legal txs [{i}/{len(good_txs)}]", end='\r')
             try:
                 response = requests.get(f"https://blockchain.info/rawtx/{good_txs['transaction'].iloc[i]}")
                 tx_json = response.json()
@@ -70,14 +70,14 @@ def getRealTransactionData(limitUnknown: int):
             except Exception:
                 txs_failed_list.append(good_txs['transaction'].iloc[i])
 
-        print(f"Finished getting legal transactions")
+        logInfo(f"Finished getting legal transactions")
 
-        print(f"-----------------------------------------")
-        print(f"Getting illegal transaction data from API")
-        print(f"-----------------------------------------")
+
+        logInfo(f"Getting illegal transaction data from API")
+
         # illegal transactions
         for i in range(len(bad_txs)):
-            print(f"Illegal txs [{i}/{len(bad_txs)}]", end='\r')
+            logInfo(f"Illegal txs [{i}/{len(bad_txs)}]", end='\r')
             try:
                 response = requests.get(f"https://blockchain.info/rawtx/{bad_txs['transaction'].iloc[i]}")
                 tx_json = response.json()
@@ -86,11 +86,8 @@ def getRealTransactionData(limitUnknown: int):
             except Exception:
                 txs_failed_list.append(bad_txs['transaction'].iloc[i])
 
-        print(f"Finished getting illegal transactions")
+        logInfo(f"Finished getting illegal transactions, writing to file...")
 
-        print(f"-----------------------------------------")
-        print(f"Writing to file")
-        print(f"-----------------------------------------")
         os.makedirs("pretrain", exist_ok=True)
         with open("pretrain/elliptic_txs.json", "w") as f:
             f.writelines(json.dumps(json_list))
@@ -98,29 +95,25 @@ def getRealTransactionData(limitUnknown: int):
         with open("pretrain/failed_txs.json", "w") as f:
             f.writelines(txs_failed_list)
     else:
-        print("File pretrain/elliptic_txs.json already exists. Skipping data fetching.")
+        logInfo("File pretrain/elliptic_txs.json already exists. Skipping data fetching.")
 
-    print(f"-----------------------------------------")
-    print(f"Getting unknown transaction data from API")
-    print(f"-----------------------------------------")
+    logInfo(f"Getting unknown transaction data from API")
 
     json_list_unknown = []
     txs_failed_list = []
     # legal transactions
     for i in range(limitUnknown):
         try:
-            print(f"Unknown txs [{i}/{limitUnknown}]", end='\r')
+            logInfo(f"Unknown txs [{i}/{limitUnknown}]", end='\r')
             response = requests.get(f"https://blockchain.info/rawtx/{unknown_txs['transaction'].iloc[i]}")
             tx_json = response.json()
             json_list_unknown.append(tx_json)
         except Exception:
             txs_failed_list.append(unknown_txs['transaction'].iloc[i])
 
-    print(f"Finished getting unknown transactions")
+    logInfo(f"Finished getting unknown transactions")
 
-    print(f"-----------------------------------------")
-    print(f"Writing to file")
-    print(f"-----------------------------------------")
+    logInfo(f"Writing to file")
 
     with open("pretrain/unknown_raw_txs.json", "w") as f:
         f.writelines(json.dumps(json_list_unknown))
@@ -128,9 +121,35 @@ def getRealTransactionData(limitUnknown: int):
     with open("pretrain/failed_txs.json", "w") as f:
         f.writelines(txs_failed_list)
 
+def flatten_txs():
+    """
+    This function reads transaction data from 'pretrain/elliptic_txs.json', flattens each transaction,
+    and writes the flattened transactions to 'pretrain/flat_txs.json'.
+    The function performs the following steps:
+    1. Opens and reads the JSON file containing transaction data.
+    2. Flattens each transaction using the `flatten` function.
+    3. Writes the flattened transactions to a new JSON file.
+    """
+
+    f = open('pretrain/elliptic_txs.json')
+    data = json.load(f)
+    flat_txs = []
+
+    logInfo(f"Flattening transaction data")
+    for tx in data:
+        flat_tx = flatten(tx)
+        flat_txs.append(flat_tx)
+
+    with open("pretrain/flat_txs.json", "w") as f:
+        json.dump(flat_txs, f, indent=2)
+
 if __name__ == '__main__':
     # Parse the arguments
     args = parseArguments()
 
     # Get real transaction data
-    getRealTransactionData(args.limitUnknown)
+    if (not args.skipAPIFetch):
+        getRealTransactionData(args.limitUnknown)
+
+    # Flatten transactions
+    flatten_txs()
